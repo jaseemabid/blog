320854c3082a3813c41836103bd2631324f50d4d
Jaseem Abid <jaseemabid@gmail.com>
2012-05-20 18:10:55 +0530

    Cleaner implementation for handling twitter handles automatically

--- js/script.js
+++ js/script.js
@@ -8,11 +8,10 @@ $(function () {
 				i;
 
 			for (i = 0; i < len; i += 1) {
-				data = data.replace(res[i], $("<div>").append(
+				data = data.replace(res[i],
 					$('<a>').attr({
 						"href": "http://twitter.com/" + res[i].slice(1)
-					}).html(res[i])
-				).html());
+					}).html(res[i]).prop('outerHTML'));
 			}
 			$(elem).html(data);
 			return this;
