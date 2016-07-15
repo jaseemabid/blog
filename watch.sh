778fb7e808f5b45520d13461229f3f00c55f2698
Jaseem Abid <jabid@fybr-tech.com>
2016-07-16 01:55:46 +0530

    rm -rf /

--- watch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# only watches first parameter for modification
-while inotifywait -e modify css/style.less; do
-        echo
-        make lessc
-        echo +++ Last run: `date`  +++
-done
